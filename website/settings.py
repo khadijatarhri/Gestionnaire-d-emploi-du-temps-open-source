a60838816d9f83f7d2dae8e8160a074e8cd1eeca
ChoroukRajat <165086687+ChoroukRajat@users.noreply.github.com>
2024-06-06 20:37:32 +0100

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -143,5 +143,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-mail.outlook.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'rajatchorouk@outlook.fr'
+EMAIL_HOST_USER = 'flopedt9@gmail.com'
 EMAIL_HOST_PASSWORD = 'c'
